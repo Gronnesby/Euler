3c1d8bb83f49d438836b9909fdf82defd4bc68cb
Gronnesby <morten.gronnesby@uit.no>
2020-10-23 12:13:25 +0200

    Added isprime function to utils

---
+++ euler_utils.c
@@ -0,0 +1,26 @@
+
+#include "euler_utils.h"
+
+
+// Primality test using 6k+-1 optimization.
+int is_prime(long num)
+{
+
+    if (num <= 3)
+    {
+        return num > 1;
+    }
+    else if (((num % 2) == 0) || ((num % 3) == 0))
+    {
+        return 0;
+    }
+    
+    for (long i = 5; (i * i) <= num; i += 6)
+    {
+        if (((num % i) == 0) || ((num % (i + 2)) == 0))
+        {
+            return 0;
+        }
+    } 
+    return 1;
+}
---
+++ euler_utils.h
@@ -0,0 +1,11 @@
+#ifndef __EULER_H__
+#define __EULER_H__
+
+
+// Primality test using 6k+-1 optimization.
+int is_prime(long num);
+
+
+
+
+#endif
