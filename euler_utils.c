6decf4d2de95cc57931f2b124fe250d562008a12
3inar <einar@cs.uit.no>
2020-11-13 13:48:35 +0100

    Add prime functionality

--- euler_utils.c
+++ euler_utils.c
@@ -1,8 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 #include "euler_utils.h"
 
 
-// Primality test using 6k+-1 optimization.
+
+unsigned int search(
+    unsigned int *array, 
+    unsigned int value, 
+    unsigned int start,
+    unsigned int end) 
+{
+    // stop condition!
+    if (start == end) {
+        return value == array[start];
+    }
+
+    // split array in two
+    int middle = (end - start)/2 + start;
+    if (value > array[middle]) {
+        return search(array, value, middle + 1, end);
+    } else {
+        return search(array, value, start, middle);
+    }
+}
+
+unsigned int search_sequential(
+    unsigned int *array, 
+    unsigned int value, 
+    unsigned int start,
+    unsigned int end) 
+{
+    int middle;
+
+    while (start != end) { // stop condition
+
+        // split array in two
+        middle = (end - start)/2 + start;
+        if (value > array[middle]) {
+            start = middle + 1;
+        } else {
+            end = middle;
+        }
+    }
+
+    return array[end] == value;
+}
+
+void generate_primes(unsigned int limit) {
+    // allocate space for our primes
+    unsigned int num_primes = 1.4*limit/log(limit);  // approximate prime counting function 
+    unsigned int* prime_array = malloc(num_primes * sizeof(unsigned int));    
+    
+    // checks all numbers from 2 to limit
+    // work can be cut in half by a simple modification (exercise for the reader)
+    unsigned int prime_count = 0;
+    for (unsigned int i = 2; i <= limit; i++) {
+        if (is_prime(i)) prime_array[prime_count++] = i;
+
+        if (!(prime_count < num_primes)) { printf("Array too small\n"); exit(-1); }
+
+        // progress counter updates every 10k 
+        if ((i % 10000) == 0) printf("\r%d %%       ", (int)((100*(float) i)/(float)limit));
+    }
+
+    // write to file
+    FILE* fpt;
+    fpt = fopen("primes.bin", "wb");
+
+    fwrite(&prime_count, sizeof(unsigned int), 1, fpt);  // writes array size
+    fwrite(prime_array, sizeof(unsigned int), prime_count, fpt); // writes data
+
+    fclose(fpt);
+
+    printf("\r wrote %d primes to primes.bin\n", prime_count);
+}
+
+void load_primes(unsigned int* count, unsigned int** buffer) {
+    FILE* fpt;
+    fpt = fopen("primes.bin", "rb");
+
+    // first read array size
+    fread(count, sizeof(unsigned int), 1, fpt);
+
+    // allocate space, read data into array
+    *buffer = malloc((* count)*sizeof(unsigned int));
+    fread(*buffer, sizeof(unsigned int), *count, fpt);
+
+    printf("read %d primes from primes.bin\n", *count);
+
+    fclose(fpt);
+}
+
 int is_prime(long num)
 {
 
--- euler_utils.h
+++ euler_utils.h
@@ -5,4 +5,25 @@
 // Primality test using 6k+-1 optimization.
 int is_prime(long num);
 
+// makes a file on disk of all primes smaller than limit
+void generate_primes(unsigned int limit);
+
+// reads a bunch of prime numbers from disk
+void load_primes(unsigned int* count, unsigned int** buffer);
+
+// recursive binary search in array of uints, returns T/F
+unsigned int search(
+    unsigned int *array, // search array
+    unsigned int value,  // query value
+    unsigned int start,  // should be first index in array (0)
+    unsigned int end);   // should be last index in array
+
+// sequential binary search in array of uints, see search()
+unsigned int search_sequential(
+    unsigned int *array, 
+    unsigned int value, 
+    unsigned int start,
+    unsigned int end);
+
+
 #endif
